bb6fd3a6d0d6ab6b6994a365fc759dab73fa693b
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-04-11 12:13:00 +0200

    feat() Users register endpoint
    Create an endpoint for users to sign in. Make the package.json as the config file for the project
    
    [FA-17]

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'jose'
@@ -62,5 +63,6 @@ class Items(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5002, debug=True)
